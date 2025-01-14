e857c529f0b9e9e6eea4ad39fa20e7100fb68cea
Venizio <gabinpincon@gmail.com>
2025-01-14 17:01:20 +0100

    [ADD]SkillTreeEditor

---
+++ UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/Leaf.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace GabinBaptisteEnguerrandProject.Scripts
+{
+    public class Leaf : MonoBehaviour
+    {
+        private string _name;
+        private int _price;
+        private bool _isLocked;
+        private Vector3 _position;
+        private int _number;
+
+        public Leaf(string name, int price, bool isLocked, Vector3 position, int number)
+        {
+            this._name = name;
+            _price = price;
+            _isLocked = isLocked;
+            _position = position;
+            _number = number;
+        }
+
+        
+    }
+}
