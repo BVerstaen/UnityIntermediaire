b4b50b55d81d954100dfa638f6d6cf3d05471607
Venizio <gabinpincon@gmail.com>
2025-01-15 16:53:55 +0100

    [MOD] SkillTree

--- UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/SkillTreeEditor.cs
+++ UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/SkillTreeEditor.cs
@@ -6,18 +6,44 @@ namespace GabinBaptisteEnguerrandProject.Scripts
     [CustomEditor(typeof(SkillTree))]
     public class SkillTreeEditor : Editor
     {
+        [SerializeField] private GameObject leafPrefab;
+        private SerializedProperty leafPrefabProperty;
+        private Vector2 position = Vector2.zero;
+
+        private void OnEnable()
+        {
+            leafPrefabProperty = serializedObject.FindProperty("leafPrefab");
+        }
+
         public override void OnInspectorGUI()
         {
+            serializedObject.Update();
+            Leaf oldLeaf = null;
+            EditorGUILayout.PropertyField(leafPrefabProperty, new GUIContent("Leaf Prefab"));
+
             var skillTree = (SkillTree)target;
+            skillTree.nameText = EditorGUILayout.TextField("Leaf name", skillTree.nameText);
+            skillTree.position = EditorGUILayout.Vector2Field("Leaf position", skillTree.position);
+            skillTree.price = EditorGUILayout.IntField("Unlock Price", skillTree.price);
+            skillTree.number = EditorGUILayout.IntField("Number of the leaf you want test", skillTree.number);
+            skillTree.isLocked = EditorGUILayout.Toggle("Is locked", skillTree.isLocked);
 
             if (GUILayout.Button("Generate Skill Tree"))
             {
-                //skillTree.AddLeaf();
+                oldLeaf = skillTree.AddLeaf(skillTree.nameText, skillTree.price, skillTree.isLocked, skillTree.position,
+                    0, oldLeaf);
             }
+
             if (GUILayout.Button("Remove Leaf"))
             {
                 skillTree.ClearLeaf();
             }
+
+            if (GUILayout.Button("UnlockLeaf"))
+            {
+                skillTree.UnlockLeaf(skillTree.number);
+            }
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
