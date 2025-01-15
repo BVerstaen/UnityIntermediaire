f6ce655da81d1eba1f8bb5ead942530cbf976235
riufix <enguerrand.cousin@gmail.com>
2025-01-15 17:00:24 +0100

    [ADD]: drag in UI

--- UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/Drag and Drop/ObjectInteraction.cs
+++ UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/Drag and Drop/ObjectInteraction.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 //------Require section------//
-[RequireComponent(typeof(InputActionReference))]
+//[RequireComponent(typeof(InputActionReference))]
 [RequireComponent(typeof(GameObject))]
-[RequireComponent(typeof(Collider2D))]
+//[RequireComponent(typeof(Collider2D))]
 
-public class ObjectInteraction : MonoBehaviour
+public class ObjectInteraction : MonoBehaviour, IPointerEnterHandler
 {
     [Header("References")]
     [SerializeField] InputActionReference _clickToDrag;
     [SerializeField] GameObject _target;
     [SerializeField] Collider2D _coll;
-    [SerializeField] LayerMask _mask;
 
     [Header("Parameters")]
     [SerializeField] bool _isDragable = true;
+    [SerializeField] LayerMask _mask;
+    [SerializeField] _mode mode;
 
     private Vector2 _initPos;
     private Vector2 _mousePos;
+    private Camera _cam;
     private Coroutine _dragUpdateCoroutine;
+    private PointerEventData _UImousPos;
 
-    // Start is called before the first frame update
+    public enum _mode
+    {
+        Game,
+        UI
+    };
+
     void Start()
     {
         _clickToDrag.action.performed += OnHold;
@@ -39,24 +49,54 @@ public class ObjectInteraction : MonoBehaviour
 
     void OnHold(InputAction.CallbackContext ctx)
     {
-        //Debug.Log("test");
-        if (_isDragable)
-            _dragUpdateCoroutine = StartCoroutine(DragUpdate());
+        switch (mode)
+        {
+            case _mode.Game:
+                if (_isDragable && )
+                    _dragUpdateCoroutine = StartCoroutine(DragUpdate());
+                break;
+            case _mode.UI:
+                Debug.Log("test");
+                if (_isDragable)
+                    _dragUpdateCoroutine = StartCoroutine(DragUpdate());
+                break;
+        }
     }
 
     void OnReleased(InputAction.CallbackContext ctx)
     {
-        var hit = Physics2D.OverlapBox(_target.transform.position, _target.transform.localScale, 0, _mask);
-
-        if(hit != null ) 
+        switch (mode)
         {
-            //if (LayerMask.GetMask(_mask))
-            _isDragable = false;
-            _target.transform.position = _initPos;
-            Debug.Log("Placement impossible");
+            case _mode.Game:
+                var hit = Physics2D.OverlapBox(_target.transform.position, _target.transform.localScale, 0, _mask);
+
+                if (hit != null)
+                {
+                    //if (LayerMask.GetMask(_mask))
+                    _isDragable = false;
+                    _target.transform.position = _initPos;
+                    Debug.Log("Placement impossible");
+                }
+                else
+                    _target.transform.position = _mousePos;
+                break;
+            case _mode.UI:
+                GraphicRaycaster UIhit = FindAnyObjectByType<GraphicRaycaster>();
+                List<RaycastResult> results = new List<RaycastResult>();
+                foreach(RaycastResult result in results)
+                {
+                    if (result.gameObject.CompareTag("InvalidPlacement"))
+                    {
+                        _isDragable = false;
+                        _target.transform.position = _initPos;
+                        Debug.Log("Placement impossible");
+                    }
+                    else
+                        _target.transform.position = _mousePos;
+                }
+                
+            break;
         }
-        else
-            _target.transform.position = _mousePos;
 
         StopAllCoroutines();
         _isDragable = true;
@@ -66,10 +106,29 @@ public class ObjectInteraction : MonoBehaviour
     {
         while (_isDragable) 
         {
-            var mousePos = Input.mousePosition;
-            _mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-            _target.transform.position = _mousePos;
+            switch (mode)
+            {
+                case _mode.Game:
+                    var mousePos = Input.mousePosition;
+                    _mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+                    _target.transform.position = _mousePos;
+                    break;
+                case _mode.UI:
+                    var uiMousePos = Input.mousePosition;
+                    var pos = new Vector2(Screen.width, Screen.height);
+                    var newMousePos = Camera.main.ScreenToViewportPoint(uiMousePos)* pos;
+                    newMousePos -= pos / 2;
+                    _target.GetComponent<RectTransform>().localPosition = newMousePos;
+                    //Debug.Log("tes la chakal");
+                    break;
+            }
+            
             yield return null;
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _target = eventData.pointerEnter;
     }
 }
