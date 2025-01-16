18528e8944d41a2df494dcec1b1d3707a9a37a47
Venizio <gabinpincon@gmail.com>
2025-01-16 19:49:50 +0100

    [MOD]SKillTree

--- UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/Player.cs
+++ UnityIntermediaire/Assets/GabinBaptisteEnguerrandProject/Scripts/Player.cs
@@ -4,6 +4,13 @@ namespace GabinBaptisteEnguerrandProject.Scripts
 {
     public class Player : MonoBehaviour
     {
-        
+        public int _money;
+        public SkillTree _skillTree;
+
+
+        public void BuySkill()
+        {
+            _skillTree.UnlockLeaf(0,_money);
+        }
     }
 }
